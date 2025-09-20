3fa2ab2f7927592b1ffd116d39b9f244a927c8c1
AkashssA <akashkskrn@gmail.com>
2025-09-20 09:59:34 +0530

    updated code

--- src/main/java/com/mtd/ecom_server/controllers/ProductController.java
+++ src/main/java/com/mtd/ecom_server/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.mtd.ecom_server.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,60 +22,48 @@ import com.mtd.ecom_server.repos.ProductRepo;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.List;
-import java.util.Optional;
-
-@RestController 
+@CrossOrigin("*")
+@RestController
 @RequestMapping("/products")
 public class ProductController {
-	private static final Logger Log=LoggerFactory.getLogger(ProductController.class);
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+
+	@Autowired ProductRepo productRepo;
+	@Tag(name = "Get All Products")
+	@GetMapping("/all")
+	public List<Product> getAllProducts() {
+		log.info("Fetching products");
+		return productRepo.findAll();
+	}
+	@Tag(name = "Add Product")
+	@PostMapping("/add")
+	public Product addProduct(@RequestBody Product newproduct) {
+		log.info("Adding product");
+		return productRepo.save(newproduct);
+	}
+	@Tag(name = "Delete Products")
+	@DeleteMapping("/product/delete/{id}")
+	public String deleteProduct(@PathVariable String id) {
+	Optional<Product> findproduct  = productRepo.findById(id);
+		if(findproduct.isEmpty()) {
+			throw new ResourceNotFoundException("Product Not Found");
+		}
+		productRepo.deleteById(id);
+
+		return "Product Deleted ";
+	}
+	@Tag(name = "Edit Products")
+	@PutMapping ("/product/edit/{id}")
+	public Product editPorduct(@PathVariable String id, @RequestBody Product newproduct) {
+		Product findproduct = productRepo.findById(id).get();
+		findproduct.setName(newproduct.getName());
+		findproduct.setDescription(newproduct.getDescription());
+		findproduct.setCategory(newproduct.getCategory());
+		findproduct.setTags(newproduct.getTags());
+		findproduct.setPrice(newproduct.getPrice());
+		findproduct.setStock(newproduct.getStock());
+		return productRepo.save(findproduct) ;
+	}
 	
 	
-    
-    @Autowired 
-    ProductRepo productRepo;
-    @Tag(name="get all products")
-     
-    @GetMapping("/all")
-    public List<Product> getAllProducts() {
-    	Log.info("fetching products");
-        return productRepo.findAll();
-    }
-
-    @PostMapping("/add")
-    public Product addProduct(@RequestBody Product newproduct) {
-    	Log.info("Adding product"+newproduct);
-        
-        return productRepo.save(newproduct);
-    }
-
-    @DeleteMapping("/product/delete/{id}")
-    public String deleteProduct(@PathVariable String id) {
-        Optional<Product> findproduct = productRepo.findById(id);
-
-        if (findproduct.isEmpty()) {
-            Log.error("Failed to delete product " + id);
-            throw new ResourceNotFoundException("product not found");
-            
-        }
-
-        productRepo.deleteById(id);
-        Log.info("Product Deleted " + id);
-       
-        return "product Deleted";
-    }
-
-
-    @PutMapping("/product/edit/{id}")
-    public Product editProduct(@PathVariable String id, @RequestBody Product newproduct) {
-        Product findproduct = productRepo.findById(id).get();
-        findproduct.setName(newproduct.getName());
-        findproduct.setDescription(newproduct.getDescription());
-        findproduct.setCategory(newproduct.getCategory());
-        findproduct.setTags(newproduct.getTags()); 
-        findproduct.setPrice(newproduct.getPrice());
-        findproduct.setStock(newproduct.getStock());
-    	Log.info("updating product"+findproduct);
-        return productRepo.save(findproduct);
-    }
 }
